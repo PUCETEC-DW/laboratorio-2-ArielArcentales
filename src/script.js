e74be7a8d1d3e6da6a5e98d3481e0bf12fe159fa
Ariel Arcentales <amarcentales@puce.edu.ec>
2025-05-21 22:28:00 -0500

    Primera Prueba de funcionalidad

--- src/script.js
+++ src/script.js
@@ -5,7 +5,41 @@ fetch("https://restcountries.com/v3.1/all")
   .then(data => {
     paises = data;
     mostrarPaises(paises); 
-  })
-  .catch(error => {
-    console.error("Error al obtener los datos:", error);
   });
+
+  let campoBuscar = document.getElementById("buscar");
+let contenedorResultado = document.getElementById("resultado");
+
+campoBuscar.addEventListener("input", () => {
+  let texto = campoBuscar.value.toLowerCase();
+  contenedorResultado.innerHTML = "";
+
+  let filtrados = paises.filter(p => 
+    p.name.official.toLowerCase().includes(texto)
+  );
+
+  filtrados.forEach(pais => {
+    let tarjeta = document.createElement("div");
+
+    let titulo = document.createElement("h2");
+    titulo.textContent = pais.name.official;
+
+    let bandera = document.createElement("img");
+    bandera.src = pais.flags.svg;
+    bandera.alt = `Bandera de ${pais.name.official}`;
+    bandera.width = 50;
+
+    let region = document.createElement("p");
+    region.textContent = `Región: ${pais.region}`;
+
+    let poblacion = document.createElement("p");
+    poblacion.textContent = `Población: ${pais.population.toLocaleString()}`;
+
+    tarjeta.appendChild(titulo);
+    tarjeta.appendChild(bandera);
+    tarjeta.appendChild(region);
+    tarjeta.appendChild(poblacion);
+
+    contenedorResultado.appendChild(tarjeta);
+  });
+});
